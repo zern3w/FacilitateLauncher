018d21b71d1578c9a3ab2696d92904239382e106
Puttipong Tadang <puttipong.tadang@clbs.co.th>
2018-02-01 03:26:48 +0700

    Updated to lanucher

---
+++ app/src/main/java/android/facilitatelauncher/Launcher.java
@@ -0,0 +1,25 @@
+package android.facilitatelauncher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by puttipongtadang on 2/1/18.
+ */
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        showApp();
+    }
+
+    private void showApp(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
