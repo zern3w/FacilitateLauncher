be9c7825caa901a723c7a2a48f3689b3c0cab2b9
Puttipong T <zEr.n3w@gmail.com>
2018-03-06 04:59:10 +0700

    Adjusted layout menu chooser

--- app/src/main/java/android/facilitatelauncher/activity/MenuChooserActivity.kt
+++ app/src/main/java/android/facilitatelauncher/activity/MenuChooserActivity.kt
@@ -1,10 +1,14 @@
 package android.facilitatelauncher.activity
 
+import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.facilitatelauncher.R
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+
+import kotlinx.android.synthetic.main.activity_menu_chooser.*
 
 class MenuChooserActivity : AppCompatActivity() {
 
@@ -14,7 +18,17 @@ class MenuChooserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_menu_chooser)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        cvElder.setOnClickListener { Log.i("test", "Elder") }
+        cvHandicap.setOnClickListener {  Log.i("test", "cvHandicap")}
+        cvExitApp.setOnClickListener { this.finishAffinity()
+            System.exit(0) }
     }
 
 
